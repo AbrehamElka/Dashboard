8f2e52fb1c06edba4a52cf2c427aeaaaad745bef
AbrehamElka <abrehamdessalegn02@gmail.com>
2025-02-11 21:28:04 +0300

    Side Bar made

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/NavBar";
+import SideBar from "@/components/SideBar";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -29,7 +31,16 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <NavBar />
-        {children}
+
+        <div className="flex">
+          <div className="hidden md:block h-[100vh] w-[300px]">
+            <SideBar />
+          </div>
+          <div className="p-5 w-full md:max-w-[1140px]">
+            {children}
+          </div>
+        </div>
+        
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <Button size="sm" variant="secondary">Hello</Button>
+      hello
     </div>
   );
 }
